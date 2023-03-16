66b5da4ceebd7cfe0865e2359320cd6419043e4d
Vanessa Hoang <56266258+vanessah9@users.noreply.github.com>
2023-03-16 14:50:32 -0400

    update 8 files

--- smart-customer-services/src/components/Login.tsx
+++ smart-customer-services/src/components/Login.tsx
@@ -5,8 +5,8 @@ interface InputProps {
 export default function Login(props: InputProps) {
   return (
     <div className="login">
-      <h1>Login</h1>
-      <form>
+      <h1 className="login-title">Login</h1>
+      <form className="login-form">
         <div className="mb-3">
           <div className="form-floating">
             <input type="email" className="form-control" id="email" />
@@ -19,8 +19,8 @@ export default function Login(props: InputProps) {
             <label htmlFor="password">Password</label>
           </div>
         </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
+        <button type="submit" className="btn btn-primary btn-lg login-btn">
+          Login
         </button>
       </form>
     </div>
