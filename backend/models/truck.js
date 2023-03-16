97e9c3fdcde287d795ffe3ac59cb4f86c9353093
Ethan Hou <48737793+ethan-hou@users.noreply.github.com>
2023-03-16 14:54:06 -0400

    modify truck model

--- backend/models/truck.js
+++ backend/models/truck.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 
 const truckSchema = new mongoose.Schema({
-  tripId: {
-    type: mongoose.SchemaTypes.ObjectId,
-    required: true,
-  },
   truckCode: {
     type: String,
     required: true,
@@ -13,32 +9,10 @@ const truckSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  status: {
-    type: String,
-    default: "Available",
-  },
   location: {
     type: String,
     required: true,
   },
-  currentStatus: {
-    type: String,
-    default: "Idle",
-  },
-  currentDistance: {
-    type: Number,
-    default: 0,
-  },
-  currentLocation: {
-    type: String,
-    default: "Idle",
-  },
-  currentDestination: {
-    type: String,
-    default: "Idle",
-  },
-  currentETD: Date,
-  currentETA: Date,
 });
 
 module.exports = mongoose.model("Truck", truckSchema);
